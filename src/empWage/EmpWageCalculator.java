1a9e57436a8f65f7617effb7e8e30b6731549e94
sahil gawathe <sahilgawathe4956@gmail.com>
2025-01-10 14:45:03 +0530

    uc1 for emp attendence

--- src/empWage/EmpWageCalculator.java
+++ src/empWage/EmpWageCalculator.java
@@ -1,6 +1,27 @@
 package empWage;
+import java.util.Random;
+
 public class EmpWageCalculator {
+	static int IS_FULL_TIME = 1;
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to Employee Wage Computation Program");
+		if(isEmpPresent()) {
+			System.out.println("Employee is Present!");
+		}
+		else {
+			System.out.println("Employee is Absent!");
+		}
+		
+	}
+	
+	private static boolean isEmpPresent() {
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if(empCheck == IS_FULL_TIME) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
