a9e31599c8ed86c77d1eabed171fe67330f36d1e
sahil gawathe <sahilgawathe4956@gmail.com>
2025-01-10 15:56:08 +0530

    if else changed to switch

--- src/empWage/EmpWageCalculator.java
+++ src/empWage/EmpWageCalculator.java
@@ -1,11 +1,11 @@
 package empWage;
-import java.util.Random;
+package empWage;
 
 
 public class EmpWageCalculator {
-	static int IS_FULL_TIME = 1;
-	static int IS_PART_TIME = 2;
-	static int EMP_RATE_PER_HOUR = 20;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
 	static int EMP_HOURS = 0;
 	static int EMP_WAGE = 0;
 	public static void main(String[] args) {
@@ -17,23 +17,25 @@ public class EmpWageCalculator {
 	}
 	
 	private static void isEmpPresent() {
-		double empCheck = Math.floor(Math.random()*10)%3;
+		int empCheck = (int) Math.floor(Math.random()*10)%3;
+		switch(empCheck) {
+			case IS_PART_TIME :
+				System.out.println("Part Time Employee is Present!");
+				EMP_HOURS = 4;
+				break;
+			case IS_FULL_TIME:
+				System.out.println("Full Time Employee is Present!");
+				EMP_HOURS = 8;
+				break;
+			default :
+				EMP_HOURS = 0;	
+		}
 		
-		if(empCheck == IS_FULL_TIME) {
-			System.out.println("Full Time Employee is Present!");
-			EMP_HOURS = 8;
-		}
-		else if(empCheck == IS_PART_TIME) {
-			System.out.println("Part Time Employee is Present!");
-			EMP_HOURS = 4;
-		}
-		else {
-			System.out.println("Employee is Absent!");
-			EMP_HOURS = 0;
-		}
 	}
+	
 	private static int calculateEmpWage() {
 		EMP_WAGE = EMP_HOURS * EMP_RATE_PER_HOUR;
 		return EMP_WAGE;
 	}
+	
 }
