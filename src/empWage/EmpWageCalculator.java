b8c145303055758b748fd95cc4d6876b2001851e
sahil gawathe <sahilgawathe4956@gmail.com>
2025-01-10 15:59:31 +0530

    fuction to cal montly wages

--- src/empWage/EmpWageCalculator.java
+++ src/empWage/EmpWageCalculator.java
@@ -1,18 +1,23 @@
 package empWage;
-package empWage;
 
 
 public class EmpWageCalculator {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUMBER_OF_WORKING_DAYS = 20;
 	static int EMP_HOURS = 0;
 	static int EMP_WAGE = 0;
+	static int TOTAL_WAGE =0;
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation Program");
-		isEmpPresent();
-		System.out.println("EMP Wage : "+calculateEmpWage());
+		for(int i =0; i< NUMBER_OF_WORKING_DAYS; i++) {
+			isEmpPresent();	
+			System.out.println("Emp Wage : "+ calculateEmpWage());
+			
+		}
+		System.out.println("Total Emp Wage : "+ TOTAL_WAGE);
 		
 	}
 	
@@ -28,6 +33,7 @@ public class EmpWageCalculator {
 				EMP_HOURS = 8;
 				break;
 			default :
+				System.out.println("Employee is Absent!");
 				EMP_HOURS = 0;	
 		}
 		
@@ -35,6 +41,7 @@ public class EmpWageCalculator {
 	
 	private static int calculateEmpWage() {
 		EMP_WAGE = EMP_HOURS * EMP_RATE_PER_HOUR;
+		TOTAL_WAGE += EMP_WAGE;
 		return EMP_WAGE;
 	}
 	
