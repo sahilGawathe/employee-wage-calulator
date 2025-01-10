44b61f7dedd45ac25faaf35b16adcec00275bbfb
sahil gawathe <sahilgawathe4956@gmail.com>
2025-01-10 12:12:01 +0530

    Displayed welcome msg on main branch

---
+++ src/empWage/EmpWageCalculator.java
@@ -0,0 +1,6 @@
+package empWage;
+public class EmpWageCalculator {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module employe_wage {
+}
