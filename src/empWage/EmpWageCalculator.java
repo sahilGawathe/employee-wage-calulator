816c34932d42a85994d8dfe1422714d76cb06f49
sahil gawathe <sahilgawathe4956@gmail.com>
2025-01-10 15:52:09 +0530

    Added:part time emp

--- src/empWage/EmpWageCalculator.java
+++ src/empWage/EmpWageCalculator.java
@@ -1,35 +1,35 @@
 package empWage;
 import java.util.Random;
 
+
 public class EmpWageCalculator {
 	static int IS_FULL_TIME = 1;
+	static int IS_PART_TIME = 2;
 	static int EMP_RATE_PER_HOUR = 20;
 	static int EMP_HOURS = 0;
 	static int EMP_WAGE = 0;
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation Program");
-		
-		if(isEmpPresent()) {
-			System.out.println("Employee is Present!");
-		}
-		
-		else {
-			System.out.println("Employee is Absent!");
-		}
+		isEmpPresent();
 		System.out.println("EMP Wage : "+calculateEmpWage());
 		
 	}
 	
-	private static boolean isEmpPresent() {
-		double empCheck = Math.floor(Math.random()*10)%2;
+	private static void isEmpPresent() {
+		double empCheck = Math.floor(Math.random()*10)%3;
+		
 		if(empCheck == IS_FULL_TIME) {
+			System.out.println("Full Time Employee is Present!");
 			EMP_HOURS = 8;
-			return true;
+		}
+		else if(empCheck == IS_PART_TIME) {
+			System.out.println("Part Time Employee is Present!");
+			EMP_HOURS = 4;
 		}
 		else {
+			System.out.println("Employee is Absent!");
 			EMP_HOURS = 0;
-			return false;
 		}
 	}
 	private static int calculateEmpWage() {
